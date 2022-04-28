e67a80de7606c48ab28adfa758cb90ff2034cc7d
Pdante1897 <66376784+Pdante1897@users.noreply.github.com>
2022-04-27 19:10:22 -0600

    Creacion de Relaciones
    Se relacionaron las tablas

--- DDL.sql
+++ DDL.sql
@@ -81,7 +81,7 @@ create table ACTA_DEFUNCION(
 );
 
 create table LICENCIA_CONDUCIR(
-	id_liecncia numeric not null primary key,
+	id_licencia numeric not null primary key,
     persona numeric not null,
     tipo_licencia numeric not null,
     fecha numeric not null
@@ -99,6 +99,83 @@ create table VIGENCIA(
     fecha_inicio numeric not null,
     fecha_expiracion numeric not null
 );
+
+
+alter table ACTA_NAC 
+ADD FOREIGN KEY (genero) REFERENCES GENERO(id_genero); 
+
+alter table ACTA_NAC 
+ADD FOREIGN KEY (padre) REFERENCES DPI(id_dpi); 
+
+alter table ACTA_NAC 
+ADD FOREIGN KEY (madre) REFERENCES DPI(id_dpi); 
+
+alter table ACTA_NAC 
+ADD FOREIGN KEY (municipio) REFERENCES MUNICIPIO(id_municipio); 
+
+alter table ACTA_NAC 
+ADD FOREIGN KEY (fecha_nac) REFERENCES FECHA(id_fecha); 
+
+alter table ACTA_NAC 
+ADD FOREIGN KEY (persona) REFERENCES PERSONA(cui); 
+
+alter table DPI
+ADD FOREIGN KEY (cui) REFERENCES PERSONA(cui); 
+
+alter table DPI
+ADD FOREIGN KEY (fecha) REFERENCES FECHA(id_fecha); 
+
+alter table DPI
+ADD FOREIGN KEY (municipio) REFERENCES MUNICIPIO(id_municipio); 
+
+alter table DPI
+ADD FOREIGN KEY (estado_civil) REFERENCES ESTADO_CIVIL(id_estado_civil); 
+
+alter table ACTA_MATRI
+ADD FOREIGN KEY (esposo) REFERENCES DPI(id_dpi); 
+
+alter table ACTA_MATRI
+ADD FOREIGN KEY (esposa) REFERENCES DPI(id_dpi); 
+
+alter table ACTA_MATRI
+ADD FOREIGN KEY (fecha) REFERENCES FECHA(id_fecha); 
+
+alter table ACTA_DIVORCIO
+ADD FOREIGN KEY (acta_matri) REFERENCES ACTA_MATRI(id_acta_matri); 
+
+alter table ACTA_DIVORCIO
+ADD FOREIGN KEY (fecha) REFERENCES FECHA(id_fecha); 
+
+alter table MUNICIPIO
+ADD FOREIGN KEY (departamento) REFERENCES DEPARTAMENTO(id_departamento); 
+
+alter table ACTA_DEFUNCION
+ADD FOREIGN KEY (fecha) REFERENCES FECHA(id_fecha); 
+
+alter table ACTA_DEFUNCION
+ADD FOREIGN KEY (municipio) REFERENCES MUNICIPIO(id_municipio); 
+
+alter table ACTA_DEFUNCION
+ADD FOREIGN KEY (persona) REFERENCES PERSONA(cui); 
+
+alter table LICENCIA_CONDUCIR
+ADD FOREIGN KEY (fecha) REFERENCES FECHA(id_fecha); 
+
+alter table LICENCIA_CONDUCIR
+ADD FOREIGN KEY (persona) REFERENCES PERSONA(cui); 
+
+alter table LICENCIA_CONDUCIR
+ADD FOREIGN KEY (tipo_licencia) REFERENCES TIPO_LICENCIA(id_tipo); 
+
+alter table VIGENCIA
+ADD FOREIGN KEY (fecha_inicio) REFERENCES FECHA(id_fecha); 
+
+alter table VIGENCIA
+ADD FOREIGN KEY (fecha_expiracion) REFERENCES FECHA(id_fecha); 
+
+alter table VIGENCIA
+ADD FOREIGN KEY (licencia_conducir) REFERENCES LICENCIA_CONDUCIR(id_licencia); 
+
 
 
 
